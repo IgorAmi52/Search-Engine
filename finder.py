547ee873372be1700695c32d95340177eb8c363a
Sergej <igorami44@gmail.com>
2024-06-25 15:50:33 +0200

    Setting up trie did you mean funcitonality

--- finder.py
+++ finder.py
@@ -7,12 +7,37 @@ class Finder:
         items = self.process_phrase(phrase)
         score = {}
 
-        for index, item in enumerate(items):
-            if index == 0:
-                score = self.get_trie_score(item)
-                graph_score = self.get_graph_score(score.keys())
-                for key in score.keys():
-                    score[key] += graph_score[key]
+        i = 0
+        operation = None
+        while i < len(items):
+            if items[i] == "&":  ### intersection
+                operation = "&"
+                continue
+            elif items[i] == "|":  ### union
+                operation = "|"
+                continue
+            elif items[i] == "!":  ### complement
+                operation = "!"
+                continue
+            else:  ### word
+                if operation is None:
+                    score = self.get_trie_score(items[i])  ### first score
+                elif operation == "&":
+                    score = self.get_intersection_score_dict(
+                        score, self.get_trie_score(items[i])
+                    )
+                elif operation == "|":
+                    score = self.get_union_score_dict(
+                        score, self.get_trie_score(items[i])
+                    )
+                elif operation == "!":
+                    score = self.get_complement_score_dict(
+                        score, self.get_trie_score(items[i])
+                    )
+            graph_score = self.get_graph_score(score.keys())
+            for key in score.keys():
+                score[key] += graph_score[key]
+            i += 1
 
     def process_phrase(self, phrase):
         raw_words = phrase.split()
@@ -32,8 +57,32 @@ class Finder:
     def keep_only_alpha(self, word):
         return "".join([char for char in word if char.isalpha()])
 
+    def get_intersection_score_dict(self, dict1, dict2):
+        for key in dict1.keys():
+            if key in dict2:
+                dict1[key] += dict2[key]
+            else:
+                dict1.pop(key)
+        return dict1
+
+    def get_union_score_dict(self, dict1, dict2):
+        for key in dict2.keys():
+            if key in dict1:
+                dict1[key] += dict2[key]
+            else:
+                dict1[key] = dict2[key]
+        return dict1
+
+    def get_complement_score_dict(self, dict1, dict2):
+        for key in dict2.keys():
+            if key in dict1:
+                dict1.pop(key)
+        return dict1
+
     def get_graph_score(self, score):
         pass
 
     def get_trie_score(self, word):
-        pass
+        ret = self.trie.search(word)
+        if ret == {}:
+            raise ValueError("Word not found in the document.")
--- trie/trie.py
+++ trie/trie.py
@@ -1,5 +1,6 @@
 class TrieNode:
     def __init__(self):
+        self.parent = None
         self.children = {}
         self.is_end_of_word = False
         self.document_ids = {}
@@ -17,6 +18,7 @@ class Trie:
             char = char.lower()
             if char not in node.children:
                 node.children[char] = TrieNode()
+            node.children[char].parent = node
             node = node.children[char]
         node.is_end_of_word = True
         if doc_id not in node.document_ids:
@@ -31,3 +33,21 @@ class Trie:
                 return {}
             node = node.children[char]
         return node.document_ids if node.is_end_of_word else {}
+
+    def find_potential_word(self, word):
+        node = self.root
+        word_shallow = True
+        for char in word:  ### find the longest prefix of the word that is in the trie
+            if char not in node.children:
+                word_shallow = False
+                break
+            node = node.children[char]
+        if not word_shallow:  ### if the word is not in the trie, find the longest prefix that is in the trie
+            while not node.is_end_of_word and node.children:
+                node = node.parent
+                word = word[:-1]
+        while not node.is_end_of_word:  ### find the word that is in the trie
+            next_char = list(node.children.keys())[0]
+            node = node.children[next_char]
+            word += next_char
+        return word
